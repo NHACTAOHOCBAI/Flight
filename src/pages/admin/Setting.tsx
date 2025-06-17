c787472a33b564e9b04ad055693bc0cec651aa0c
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-06-17 15:47:01 +0700

    fix: last commit........

--- src/pages/admin/Setting.tsx
+++ src/pages/admin/Setting.tsx
@@ -69,7 +69,11 @@ export default function Setting() {
                             <Form.Item<Parameter>
                                 label="Max Intermediate Airport Quantity"
                                 name="maxInterQuantity"
-                                rules={[{ required: true }]}
+                                rules={[
+                                    { required: true, message: 'Please enter the maximum number of intermediate airports' },
+                                    { type: 'integer', message: 'Must be an integer' },
+                                    { validator: (_, value) => value >= 0 ? Promise.resolve() : Promise.reject('Value must be non-negative') }
+                                ]}
                             >
                                 <InputNumber min={0} disabled={!canUpdate} />
                             </Form.Item>
@@ -77,7 +81,11 @@ export default function Setting() {
                             <Form.Item<Parameter>
                                 label="Min Flight Time (minutes)"
                                 name="minFlightTime"
-                                rules={[{ required: true }]}
+                                rules={[
+                                    { required: true, message: 'Please enter the minimum flight time' },
+                                    { type: 'integer', message: 'Must be an integer' },
+                                    { validator: (_, value) => value >= 0 ? Promise.resolve() : Promise.reject('Time must be non-negative') }
+                                ]}
                             >
                                 <InputNumber min={0} disabled={!canUpdate} />
                             </Form.Item>
@@ -85,7 +93,19 @@ export default function Setting() {
                             <Form.Item<Parameter>
                                 label="Min Stop Time (minutes)"
                                 name="minStopTime"
-                                rules={[{ required: true }]}
+                                rules={[
+                                    { required: true, message: 'Please enter the minimum stop time' },
+                                    { type: 'integer', message: 'Must be an integer' },
+                                    {
+                                        validator: async (_, value) => {
+                                            const maxStopTime = form.getFieldValue('maxStopTime');
+                                            if (value >= 0 && (maxStopTime === undefined || value < maxStopTime)) {
+                                                return Promise.resolve();
+                                            }
+                                            return Promise.reject('Min stop time must be non-negative and less than max stop time');
+                                        }
+                                    }
+                                ]}
                             >
                                 <InputNumber min={0} disabled={!canUpdate} />
                             </Form.Item>
@@ -93,7 +113,19 @@ export default function Setting() {
                             <Form.Item<Parameter>
                                 label="Max Stop Time (minutes)"
                                 name="maxStopTime"
-                                rules={[{ required: true }]}
+                                rules={[
+                                    { required: true, message: 'Please enter the maximum stop time' },
+                                    { type: 'integer', message: 'Must be an integer' },
+                                    {
+                                        validator: async (_, value) => {
+                                            const minStopTime = form.getFieldValue('minStopTime');
+                                            if (value >= 0 && (minStopTime === undefined || value > minStopTime)) {
+                                                return Promise.resolve();
+                                            }
+                                            return Promise.reject('Max stop time must be non-negative and greater than min stop time');
+                                        }
+                                    }
+                                ]}
                             >
                                 <InputNumber min={0} disabled={!canUpdate} />
                             </Form.Item>
@@ -101,7 +133,11 @@ export default function Setting() {
                             <Form.Item<Parameter>
                                 label="Latest Booking Time (days)"
                                 name="latestBookingDay"
-                                rules={[{ required: true }]}
+                                rules={[
+                                    { required: true, message: 'Please enter the latest booking time' },
+                                    { type: 'integer', message: 'Must be an integer' },
+                                    { validator: (_, value) => value >= 0 ? Promise.resolve() : Promise.reject('Time must be non-negative') }
+                                ]}
                             >
                                 <InputNumber min={0} disabled={!canUpdate} />
                             </Form.Item>
@@ -109,7 +145,19 @@ export default function Setting() {
                             <Form.Item<Parameter>
                                 label="Latest Cancel Time (days)"
                                 name="latestCancelDay"
-                                rules={[{ required: true }]}
+                                rules={[
+                                    { required: true, message: 'Please enter the latest cancellation time' },
+                                    { type: 'integer', message: 'Must be an integer' },
+                                    {
+                                        validator: async (_, value) => {
+                                            const latestBookingDay = form.getFieldValue('latestBookingDay');
+                                            if (value >= 0 && (latestBookingDay === undefined || value <= latestBookingDay)) {
+                                                return Promise.resolve();
+                                            }
+                                            return Promise.reject('Cancellation time must be non-negative and not exceed booking time');
+                                        }
+                                    }
+                                ]}
                             >
                                 <InputNumber min={0} disabled={!canUpdate} />
                             </Form.Item>
@@ -117,12 +165,15 @@ export default function Setting() {
                             <Form.Item<Parameter>
                                 label="Refund percentage (%)"
                                 name="refundRate"
-                                rules={[{ required: true }]}
+                                rules={[
+                                    { required: true, message: 'Please enter the refund percentage' },
+                                    { validator: (_, value) => value >= 0 && value <= 100 ? Promise.resolve() : Promise.reject('Percentage must be between 0 and 100') }
+                                ]}
                             >
                                 <InputNumber min={0} disabled={!canUpdate} />
                             </Form.Item>
-                            {
-                                canUpdate &&
+
+                            {canUpdate && (
                                 <div className="2-full flex gap-[10px] justify-end">
                                     <Button onClick={() => refetchParameters()}>
                                         {icons.reset} Reset
@@ -131,7 +182,7 @@ export default function Setting() {
                                         Update
                                     </Button>
                                 </div>
-                            }
+                            )}
                         </Form>
                     </div>
             }
